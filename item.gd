f861f9432791b18f041970cb76b9f4968e6ac55a
Mangmasta <mangmasta@gmail.com>
2021-01-14 03:02:50 -0800

    Added easy way in the item's script exports to change the texture used for the item.

--- item.gd
+++ item.gd
@@ -6,11 +6,13 @@ extends Node2D
 # var b = "text"
 export(String) var Name = "Temp" #Name as how its displayed in the inventory.
 export(String) var internalName = "temp" #How to refer to it in code.  Make it without spaces and all lowercase.
-
+export(Texture) var texture setget updateTexture
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func updateTexture(newTexture):
+	get_node("Sprite").texture = newTexture
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
