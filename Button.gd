4df25de0c8707bce872022d910b7c0b6bb6c469c
MrDandey <jokaiocom@gmail.com>
2021-01-25 19:18:59 -0800

    Button And Menu
    Added a button scene for universal use. And a menu scene.

---
+++ Button.gd
@@ -0,0 +1,23 @@
+extends Button
+
+export var reference_path = ""
+export(bool) var start_focused = false
+
+signal press()
+
+func _ready():
+	if(start_focused):
+		grab_focus()
+		
+	connect("mouse_entered",self,"on_Button_mouse_entered")
+	connect("pressed",self, "_on_Button_Pressed")
+
+func _on_Button_mouse_entered():
+	grab_focus()
+
+func _on_Button_Pressed():
+	if(reference_path != ""):
+		get_tree().change_scene(reference_path)
+	else:
+		get_tree().quit()
+	emit_signal("press")
